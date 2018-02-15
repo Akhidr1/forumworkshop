d8cd63df1fcdb12936c73bbee870a08ce64d9819
Alaa Khidr <alaa.sayed.hassan@gmail.com>
2018-02-15 12:20:43 +0200

    Add member + post stores (Code not excuting)

--- forums/main.py
+++ forums/main.py
@@ -11,10 +11,29 @@ post2 = models.Post("Hi!", "First time for me here!")
 post3 = models.Post("Howdy!", "Glad to be on of your community!")
 
 
-print post3.body
 
-print post1.title
+models.MemberStore.add(member1)
+models.MemberStore.add(member2)
 
-print member1.age
+print(models.MemberStore.get_all())
 
-print member2.name
+models.PostrStore.add(post1)
+models.PostStore.add(post2)
+models.PostStore.add(post3)
+
+print(models.PostStore.get_all())
+
+
+
+
+
+
+
+
+
+
+
+#print post3.body
+#print post1.title
+#print member1.age
+#print member2.name
--- forums/models.py
+++ forums/models.py
@@ -10,3 +10,36 @@ class Post():
         self.title = title
         self.body = body
 
+class MemberStore:
+
+  members = []
+
+  def get_all(self):
+      # get all members
+
+      for member in MemberStore.members:
+
+            print(member)
+
+  def add(self, member):
+      # append member
+
+     MemberStore.members = MemberStore.members.append(self.name)
+
+
+class PostStore:
+
+  posts = []
+
+  def get_all(self):
+      # get all posts
+
+      for post in PostStore.posts:
+
+            print(post)
+
+  def add(self, post):
+      # append post
+
+     PostStore.posts = PostStore.posts.append(self.post)
+
